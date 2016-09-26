af8be5a2393b583ba8f521731133260f2e6b1fb4
Robyn Silber <rjsilber@gmail.com>
2016-09-26 10:04:49 -0400

    App before Widget has been added

---
+++ StickyNotesWidgetApp/app/src/main/java/comgw_mobile_16_fallwidgetapp/github/stickynoteswidgetapp/MainActivity.java
@@ -0,0 +1,13 @@
+package comgw_mobile_16_fallwidgetapp.github.stickynoteswidgetapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
